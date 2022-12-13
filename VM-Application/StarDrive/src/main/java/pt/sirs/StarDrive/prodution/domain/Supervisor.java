91c5e3a4b0a87ca709e14727627a4bf52415ca59
diogomsilvaa <silva.diogo.2001@gmail.com>
2022-12-13 17:03:11 +0000

    fix

--- VM-Application/StarDrive/src/main/java/pt/sirs/StarDrive/prodution/domain/Supervisor.java
+++
@@ -1,9 +0,0 @@
-package pt.sirs.StarDrive.prodution.domain;
-
-public class Supervisor {
-    public Supervisor(){}
-
-    public AssemblyLine createAssemblyLine(){
-        return new AssemblyLine();
-    }
-}
