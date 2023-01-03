8101d7ca618bc538076ae1bd95e9c4999c9dbab5
diogomsilvaa <silva.diogo.2001@gmail.com>
2023-01-03 23:41:18 +0000

    update on AuthService

--- VM-Application/StarDrive/src/main/java/pt/sirs/app/StarDrive/auth/AuthService.java
+++ VM-Application/StarDrive/src/main/java/pt/sirs/app/StarDrive/auth/AuthService.java
@@ -1,15 +1,21 @@
 package pt.sirs.app.StarDrive.auth;
 
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeoutException;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.stereotype.Service;
 
 public class AuthService {
     
     private String token;
     private Key aesKey;
     private String message;
+    private String id;
 
     public AuthService(String _token){
         setToken(_token);
@@ -19,28 +25,49 @@ public class AuthService {
 
     
     
-    public void decrypt() throws Exception{
+    private void decrypt() throws Exception{
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, aesKey);
         byte[] encrypted = cipher.doFinal(getToken().getBytes());
-        message = new String(cipher.doFinal(encrypted));
+        message = new String(encrypted);
     }
     
+    
+    private boolean verifyTimeStamp(){
+        String loginTimeStr = message.split(":")[1];
+        String expirationTimeStr = message.split(":")[2];
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime loginTime = LocalDateTime.parse(loginTimeStr, formatter);
+        LocalDateTime expirationTime = LocalDateTime.parse(expirationTimeStr, formatter);
+
+        if (loginTime.isBefore(LocalDateTime.now()) && expirationTime.isAfter(LocalDateTime.now())) {
+            return true;
+        }
+        return false;
+    }
+
+    public String VerifyToken() throws Exception{
+        this.decrypt();
+        // parse the token and check if its well built
+        if(!this.verifyTimeStamp()) throw new TimeoutException();
+        return this.getId();
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId(){
+        if(this.id == null)
+            this.id = message.split(":")[0];
+        return this.id;
+    }
+
     public void setToken(String _token) {
         this.token = _token;
     }
     
     public String getToken() {
         return token;
-    }
-
-    public boolean VerifyTimeStamp(){
-        
-        
-        // Verificar a timestamp
-
-        // Obter user
-        
-        return true;
     }
 }
--- VM-Application/StarDrive/src/main/java/pt/sirs/app/StarDrive/user/UserService.java
+++ VM-Application/StarDrive/src/main/java/pt/sirs/app/StarDrive/user/UserService.java
@@ -1,8 +1,16 @@
 package pt.sirs.app.StarDrive.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.concurrent.TimeoutException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import pt.sirs.app.StarDrive.auth.AuthService;
 import pt.sirs.app.StarDrive.user.domain.Employee;
 import pt.sirs.app.StarDrive.user.domain.Engineer;
 import pt.sirs.app.StarDrive.user.domain.User;
@@ -19,8 +27,22 @@ public class UserService {
     public UserService(){
     }
 
-    public User getUser(String id){
-        return userRepository.findById(id).orElse(null);
+    public User getUser(@RequestParam String token){
+        AuthService auth = new AuthService(token);
+        String id = null;
+        try {
+            id = auth.VerifyToken();
+        } catch (TimeoutException e) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(408));
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(500));
+        }
+
+        User user = userRepository.findById(id).orElse(null);
+        
+        if(user == null) throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+
+        return user;
     }
 
     public User createUser(String name, User.Role role){
