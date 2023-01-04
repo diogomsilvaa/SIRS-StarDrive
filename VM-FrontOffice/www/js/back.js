71c86109cffa1e5d51ca65691bc67601ec8faeb6
Carlos Amaro <carlosamaro@MacBook-Pro-de-Carlos.local>
2023-01-04 15:06:18 +0000

    some things in front end

--- VM-FrontOffice/www/js/back.js
+++ VM-FrontOffice/www/js/back.js
@@ -94,20 +94,20 @@ function login(){
         var pass = prompt("Password");
         var password = "admin";
 
-        // data = {username: login, password: pass}
-        // const response = await fetch(/*url*/,{
-        //     method: 'GET',
-        //     mode: 'cors', // no-cors, *cors, same-origin
-        //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //     credentials: 'same-origin', // include, *same-origin, omit
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //         // 'Content-Type': 'application/x-www-form-urlencoded',
-        //     },
-        //     redirect: 'follow', // manual, *follow, error
-        //     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        //     body: JSON.stringify(data) // body data type must match "Content-Type" header
-        // });
+        data = {id: login, pass: pass}
+        const response = await fetch(/*url*/,{
+            method: 'GET',
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
         
         // if(response!=="err"){
         
@@ -184,3 +184,4 @@ function machinesData(){
     console.log([abs2,abs3])
     tableMachines("machineTable",[abs2,abs3]);
 }
+
