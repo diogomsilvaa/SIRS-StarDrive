a47fd3136b1a387be62aa70cdb50c1972e78823b
diogomsilvaa <silva.diogo.2001@gmail.com>
2023-01-05 18:47:43 +0000

    vm-app conf

--- VM-Application/init.sh
+++ VM-Application/init.sh
@@ -2,6 +2,8 @@ sudo apt update
 sudo apt upgrade
 sudo apt autoremove
 
+cat toBash.txt >> ~/.bashrc
+
 sudo apt install openjdk-17-jdk
 # Fazer as alterações no bashrc
 # export JAVA_HOME=/lib/jvm/java-17-openjdk-amd64
@@ -20,3 +22,10 @@ wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+keytool -genkeypair -alias baeldung -keyalg RSA -keysize 4096 \
+  -validity 3650 -dname "CN=localhost" -keypass changeit -keystore keystore.p12 \
+  -storeType PKCS12 -storepass changeit
+
+# RESTART
+# sudo systemctl start mongodb
