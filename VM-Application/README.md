0167717a87128f40e4e7b9f41247f14fa11b1ff1
diogomsilvaa <silva.diogo.2001@gmail.com>
2023-01-05 19:04:37 +0000

    change mongo installation

--- VM-Application/README.md
+++ VM-Application/README.md
@@ -62,10 +62,10 @@ export "$M2_HOME/bin:$PATH"
 Run this commands to install and initialize mongodb
 
 ```
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install mongodb-org=4.4.8 mongodb-org-server=4.4.8 mongodb-org-shell=4.4.8 mongodb-org-mongos=4.4.8 mongodb-org-tools=4.4.
 sudo systemctl start mongod
 ```
 
--- VM-Application/init.sh
+++ VM-Application/init.sh
@@ -3,6 +3,7 @@ sudo apt upgrade
 sudo apt autoremove
 
 cat toBash.txt >> ~/.bashrc
+source ~/.bashrc	
 
 sudo apt install openjdk-17-jdk
 # Fazer as alterações no bashrc
@@ -18,10 +19,11 @@ mv apache-maven-3.8.7 /opt/
 # export M2_HOME=/opt/apache-maven-3.8.7
 # export PATH=$M2_HOME/bin:$PATH
 
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install mongodb-org=4.4.8 mongodb-org-server=4.4.8 mongodb-org-shell=4.4.8 mongodb-org-mongos=4.4.8 mongodb-org-tools=4.4.
+sudo systemctl start mongod
 
 keytool -genkeypair -alias baeldung -keyalg RSA -keysize 4096 \
   -validity 3650 -dname "CN=localhost" -keypass changeit -keystore keystore.p12 \
